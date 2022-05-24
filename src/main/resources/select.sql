c5c6b7a2c381f3517b54c45dfd83c446bc08d256
markmw <dant1kkz@gmail.com>
2022-05-24 15:40:32 +0600

    2. SQL Select by company [#2760]

---
+++ src/main/resources/select.sql
@@ -0,0 +1,17 @@
+#1
+select person.name, company.name 
+from person 
+join company on person.company_id = company.id 
+where company_id != 5;
+
+#2
+select company.name, count(person.company_id) 
+from person join company on person.company_id = company.id
+group by company.id
+having count(person.company_id) in (
+	select COUNT(company_id)
+	from person
+	group by company_id
+	order by company_id ASC
+	LIMIT 1);
+
