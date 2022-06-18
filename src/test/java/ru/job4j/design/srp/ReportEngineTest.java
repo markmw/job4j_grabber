7ca08f9528600afc83a5591ce6d98c0faf2517b8
markmw <dant1kkz@gmail.com>
2022-06-18 14:12:04 +0600

    1. Отчеты. [#850]

---
+++ src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -0,0 +1,94 @@
+package ru.job4j.design.srp;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static ru.job4j.design.srp.ReportEngine.DATE_FORMAT;
+
+public class ReportEngineTest {
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(DATE_FORMAT.format(worker.getHired().getTime())).append(";")
+                .append(DATE_FORMAT.format(worker.getFired().getTime())).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenReportToAccountant() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportAccounting(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary() / ReportAccounting.EUR).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenReportToHR() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        Employee worker1 = new Employee("Ivan", now, now, 300);
+        store.add(worker);
+        store.add(worker1);
+        Report engine = new ReportHR(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append(";")
+                .append(worker1.getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+
+    @Test
+    public void whenReportToDeveloper() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportDev(store);
+        StringBuilder expect = new StringBuilder()
+                .append("<html><table><body>"
+                        + "<tr>"
+                        + "<th>Name</th>"
+                        + "<th>Hired</th>"
+                        + "<th>Fired</th>"
+                        + "<th>Salary</th>"
+                        + "</tr>").append(System.lineSeparator())
+                .append("<tr>")
+                .append("<td>").append(worker.getName()).append("</td>")
+                .append("<td>").append(worker.getHired()).append("</td>")
+                .append("<td>").append(worker.getFired()).append("</td>")
+                .append("<td>").append(worker.getSalary()).append("</td>")
+                .append("</tr>")
+                .append(System.lineSeparator())
+                .append("</body></table></html>");
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
